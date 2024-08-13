6b8631e8b85119833f5f884e3cdbcb4907884f99
egp4aq <epeterson2708@gmail.com>
2024-08-13 12:26:25 -0700

    more stuff

--- r-py-data-types.R
+++ r-py-data-types.R
@@ -102,3 +102,33 @@ fruit[2,1] <- "pineapple"
 fruit$type
 fruit$mass
 
+
+# atomic vectors ----
+
+x <- c(2.1, 4.2, 3.3, 5.4)
+
+x[c(3,1)]
+
+x[order(x)]
+x[c(1,1)]
+x[c(2.1,2.9)]
+
+x[-1]
+x[-c(3,1)]
+
+x[c(TRUE, TRUE, FALSE, FALSE)]
+x[x > 3]
+
+x[c(TRUE, FALSE)]
+x[c(TRUE, TRUE, NA, FALSE)]
+x[]
+
+x <- 1:5
+x
+x[c(1,2)] <- 2
+x[-1] <- 4:1
+x
+x[c(TRUE, FALSE, NA)] <- 1
+x
+df <- data.frame(a=c(1,10,NA))
+df$a[df$a < 5] <- 0
